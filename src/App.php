153f3d1bcd31c785bbb7b17485cf7ac16a95fc7f
arzola <oscararzola@gmail.com>
2022-11-23 15:17:52 -0500

    feat: trigger composer update

--- src/App.php
+++ src/App.php
@@ -4,6 +4,6 @@ namespace Pressbooks\FakePlugin;
 
 class App {
     public function __construct() {
-        echo 'Hello World 2!';
+        echo 'Hello World 3! I should trigger a composer update.';
     }
 }
