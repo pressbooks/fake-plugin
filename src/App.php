03739aea71d8e8bafd4f8f9038cfd193c86caf35
arzola <oscararzola@gmail.com>
2023-02-06 13:51:46 -0500

    chore: trigger composer autoupdate

--- src/App.php
+++ src/App.php
@@ -4,6 +4,6 @@ namespace Pressbooks\FakePlugin;
 
 class App {
     public function __construct() {
-        echo 'Hello World 3! I should trigger a composer update :).';
+        echo 'Hello World 4! I should trigger a composer update :).';
     }
 }
