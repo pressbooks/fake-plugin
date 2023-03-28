e5c75d96998e080217e94d6ca0121cd95642b058
arzola <oscararzola@gmail.com>
2023-03-28 16:18:47 -0400

    fix: testing another commit

--- src/App.php
+++ src/App.php
@@ -4,6 +4,6 @@ namespace Pressbooks\FakePlugin;
 
 class App {
     public function __construct() {
-        echo 'Trigger latest version of FakePlugin App TEST PAT';
+        echo 'Trigger latest version of FakePlugin App trigger other plugins to load.';
     }
 }
