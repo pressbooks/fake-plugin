80c8c1c9a3e86cb29a43fd3473c639a3f1237f9c
arzola <oscararzola@gmail.com>
2023-04-13 15:55:09 -0400

    fix: auto review testing branch

--- src/App.php
+++ src/App.php
@@ -4,6 +4,6 @@ namespace Pressbooks\FakePlugin;
 
 class App {
     public function __construct() {
-        echo 'Trigger latest version of FakePlugin App trigger other plugins to load. auto review list.';
+        echo 'Trigger latest version of FakePlugin App trigger other plugins to load. auto review now.';
     }
 }
