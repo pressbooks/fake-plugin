b3a08088cbc6ae87d30df289b953ad3edf4ec9c3
arzola <oscararzola@gmail.com>
2023-04-13 15:25:24 -0400

    fix: auto review

--- src/App.php
+++ src/App.php
@@ -4,6 +4,6 @@ namespace Pressbooks\FakePlugin;
 
 class App {
     public function __construct() {
-        echo 'Trigger latest version of FakePlugin App trigger other plugins to load. production version.';
+        echo 'Trigger latest version of FakePlugin App trigger other plugins to load. auto review.';
     }
 }
