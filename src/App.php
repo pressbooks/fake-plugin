352ad62f3f30edf09fd30cae50cf3ebb73eebf8e
Oscar Arzola <oscararzola@gmail.com>
2023-05-02 15:22:52 -0400

    chore: lint required branch name (#9)

--- src/App.php
+++ src/App.php
@@ -4,6 +4,6 @@ namespace Pressbooks\FakePlugin;
 
 class App {
     public function __construct() {
-        echo 'Trigger latest version of FakePlugin App trigger other plugins to load. auto review now.';
+        echo 'Trigger latest version of FakePlugin App trigger other plugins to load. auto review now. hello';
     }
 }
