9a1af7535a3a48c211138d71c293118960dc263c
Oscar Arzola <oscararzola@gmail.com>
2024-04-16 17:49:46 -0400

    feat: translation files

---
+++ fake-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Fake Plugin
+ * Plugin URI: https://pressbooks.org
+ * Description: Plugin for testing purposes.
+ * // x-release-please-start-version
+ * Version: 1.0.0
+ * // x-release-please-end
+ * Requires at least: 6.5.2
+ * Requires PHP: 8.1
+ * Author: Pressbooks (Book Oven Inc.)
+ * Author URI: https://pressbooks.org
+ * Text Domain: pressbooks-fake-plugin
+ * License: GPL v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Network: True
+ */
