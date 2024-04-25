f14f4bfe340e331c69766ccd45af581b04629fee
Oscar Arzola <oscararzola@gmail.com>
2024-04-25 11:51:12 -0400

    fix: typo on world (#50)

--- src/App.php
+++ src/App.php
@@ -6,7 +6,7 @@ class App
 {
     public function __construct()
     {
-        echo __('Hello, world!', 'pressbooks-fake-plugin');
+        echo __('Hello, people!', 'pressbooks-fake-plugin');
     }
 
     public function __toString()
