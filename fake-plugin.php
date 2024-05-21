6ee18c5e3907793c5031a878e5e2079aac89d883
pressbooks-ops <66340483+pressbooks-ops@users.noreply.github.com>
2024-05-21 17:06:05 -0400

    chore(dev): release 1.9.6 (#97)
    Co-authored-by: GitHub Actions <actions@github.com>

--- fake-plugin.php
+++ fake-plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://pressbooks.org
  * Description: Plugin for testing purposes.
  * x-release-please-start-version
- * Version: 1.9.5
+ * Version: 1.9.6
  * x-release-please-end
  * Requires at least: 6.5.2
  * Requires PHP: 8.1
