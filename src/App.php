5cf33551b30d9d91472db902ea6db39470d74979
pressbooks-ops <66340483+pressbooks-ops@users.noreply.github.com>
2025-02-12 01:16:49 +0800

    chore(dev): release 1.9.9

--- src/App.php
+++ src/App.php
@@ -33,7 +33,7 @@ class App
     public function getVersion()
     {
         // x-release-please-start-version
-        return '1.9.8';
+        return '1.9.9';
         // x-release-please-end
     }
 }
