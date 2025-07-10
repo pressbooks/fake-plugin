c0f3c966b216c2481fa533fdde636df6f0ef8828
Oscar Arzola <oscararzola@gmail.com>
2025-07-10 14:05:38 -0400

    feat: new translations

--- src/App.php
+++ src/App.php
@@ -6,34 +6,43 @@ class App
 {
     public function __construct()
     {
-        echo __('Hello, people!', 'pressbooks-fake-plugin');
+        echo __("Hello, people!", "pressbooks-fake-plugin");
     }
 
     public function __toString()
     {
-        return __('Hello, world 2!', 'pressbooks-fake-plugin');
+        return __("Hello, world 2!", "pressbooks-fake-plugin");
     }
 
     public function hi()
     {
-        return __('Hello, bye!', 'pressbooks-fake-plugin');
+        return __("Hello, bye!", "pressbooks-fake-plugin");
     }
 
     public function bye()
     {
-        return __('oh la la', 'pressbooks-fake-plugin');
+        return __("oh la la", "pressbooks-fake-plugin");
     }
 
     public function salutation()
     {
-        return __('Salut! garçon', 'pressbooks-fake-plugin');
+        return __("Salut! garçon", "pressbooks-fake-plugin");
     }
-
 
     public function getVersion()
     {
         // x-release-please-start-version
-        return '1.9.15';
+        return "1.9.15";
         // x-release-please-end
+    }
+
+    public function getMouseSalutation()
+    {
+        return __("Salut! souris", "pressbooks-fake-plugin");
+    }
+
+    public function getCoffeeSalutation()
+    {
+        return __("Salut! café", "pressbooks-fake-plugin");
     }
 }
